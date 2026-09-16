a47881512249b27822ca085bb672be5f1ebff5b2
vishal singh <vks370808@gmail.com>
2026-09-16 17:15:36 +0530

    3

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('taskInput')
+const addBtn = document.getElementById('addBtn')
+const taskList = document.getElementById('taskList')
+
+function savetodos(){
+    
+}
