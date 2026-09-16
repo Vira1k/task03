ea5fa4a5ed42e05ac70e536275f71a2127b3e98c
Vishal Singh <vks370808@gmail.com>
2026-09-16 22:57:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById('taskInput')
-const addBtn = document.getElementById('addBtn')
-const taskList = document.getElementById('taskList')
-
-function savetodos(){
-    
-}
